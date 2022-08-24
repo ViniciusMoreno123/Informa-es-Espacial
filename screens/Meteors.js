564e556d9cdee5c113751c0c3548222e7749d7a7
ViniciusMoreno123 <vinisrocha8@gmail.com>
2022-08-23 20:44:21 -0400

    Versão 2.0.5 Primeira configuração da tela

--- screens/Meteors.js
+++ screens/Meteors.js
@@ -1,8 +1,56 @@
+import axios from "axios";
 import React,{Component} from "react";
-import { Text,View } from "react-native";
+import { Alert, Text,View,StyleSheet } from "react-native";
 
 export default class MeteorsScreen extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+    meteors:{}
+    }
+  }
+    getMeteors = () =>{
+    axios
+    .get("https://api.nasa.gov/neo/rest/v1/feed?api_key=75xmhoGLLPUhVcNdIEyR9BWl0sT8oJAjIc0JbOla")
+    .then(response =>{
+    this.setState.state({meteors: response.data.near_earth_objects})
+    })
+    .catch(error =>{
+        Alert.alert(error.message);
+    })
+    }
+
+    componentDidMount(){
+        this.getMeteors();
+    }
     render(){
+        if(Object.keys(this.state.meteors).length=== 0){
+            return(
+                <View style = {styles.loading}>
+                <Text>Carregando...</Text>
+                </View>
+            )
+        }else {
+        let matrizMeteor = Object.keys(this.state.meteors).map(dadosMeteors =>{
+        return this.state.meteors[dadosMeteors]
+        })
+        let meteors = [].concat.apply([],matrizMeteor)
+              
+        meteors.forEach(function(element){
+        let diametroMedio = 
+        (element.estimated_diameter.kilometers.estimated_diameter_min +
+        element.estimated_diameter.kilometers.estimated_diameter_max)/2;
+
+        let pontAmeaca = 
+        (diametroMedio /element.close_approach_data[0].miss_distance.kilometers)
+        *1000000000;
+
+        element.ameacaPont = pontAmeaca
+        })
+        
+       
+
+
         return(
         <View style = {{
             flex:1,
@@ -15,7 +63,14 @@ export default class MeteorsScreen extends Component{
         )
     }
 }
-
+}
+const styles = StyleSheet.create({
+loading:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center"
+}
+})
 
 
 
